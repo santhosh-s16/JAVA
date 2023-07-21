5535ec5c8b2495ff06c8dfa856dff5b989f6b41b
santhosh-s16 <110711155+santhosh-s16@users.noreply.github.com>
2023-07-21 01:30:37 -0700

    Add files via upload
    Write a program to find the double of the given number without using arithmetic operator.

---
+++ double.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class doubles
+{
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the number: ");
+int num = sc.nextInt();
+System.out.println(num<<2);
+}
+}

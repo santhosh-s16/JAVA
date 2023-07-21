ec5a45e7990eb08da6853021ba7f1d1095c9ca59
santhosh-s16 <110711155+santhosh-s16@users.noreply.github.com>
2023-07-21 01:32:27 -0700

    doubles.java

--- double.java
+++ double.java
@@ -6,6 +6,6 @@ public static void main(String[] args)
 Scanner sc=new Scanner(System.in);
 System.out.println("Enter the number: ");
 int num = sc.nextInt();
-System.out.println(num<<2);
+System.out.println(num<<1);
 }
 }
